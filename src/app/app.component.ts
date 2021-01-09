2a6b3e2010129581317ef6c4562064476f68a0fc
Dmitry <dmitry.zaytsev83@gmail.com>
2021-01-09 19:36:18 +0300

    replace svg logo to jpg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
